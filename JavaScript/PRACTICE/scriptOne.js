1b8b32e6562cf6a85a40516205551c29f697859e
SM. Shameem Ahamed <rony.shameem@gmail.com>
2021-02-14 21:19:09 +0600

    initial notes on javascript, reference w3schools

---
+++ JavaScript/PRACTICE/scriptOne.js
@@ -0,0 +1,12 @@
+function funcOne() {
+	var elementOne = document.getElementById("paraOne");
+	elementOne.innerHTML = "Content From JavaScript";
+}
+
+function funcTwo() {
+	var elementOne = document.getElementById("paraOne");
+	elementOne.style.backgroundColor = "#444";
+	elementOne.style.border = "solid 1px red";
+	elementOne.style.padding = "15px";
+	elementOne.style.color = "#EEE";
+}
